9d720c8dd0c6e8c19f390285b10c5915b3d35c56
Yuma Ohgami <yuma@ohgami.jp>
2018-04-01 17:27:05 +0900

    edit

--- batch_img.sh
+++ batch_img.sh
@@ -11,12 +11,16 @@ DENSITY=350
 imglst=$(grep '^//image' *.re | cut -d '[' -f 2 | cut -d ']' -f1 | sort | uniq)
 
 for img in $imglst; do
-	# echo "img: $img"
 	src=$(echo $SRCDIR/$img.*)
-	# echo "src: $src"
-	name=$(echo $src | rev | cut -d '/' -f 1 | rev)
-	dst=$DSTDIR/$name
-	# echo "dst: $dst"
+	# name=$(echo $src | rev | cut -d '/' -f 1 | rev)
+	name=$(basename $src)
+	suff=$(echo $name | rev | cut -d'.' -f1 | rev)
+	if [ "$suff" = "svg" ]; then
+		dst=$DSTDIR/$(echo $name | rev | cut -d'.' -f2- | rev).png
+	else
+		dst=$DSTDIR/$name
+	fi
+	echo "src=$src, suff=$suff, dst=$dst"
 
 	com="convert $src -page $(identify $src | cut -d ' ' -f 3)+0+0 -resize ${WIDTH}x$HEIGHT -units PixelsPerInch -resample 350 -type GrayScale -resize ${WIDTH}x$HEIGHT $dst"
 
